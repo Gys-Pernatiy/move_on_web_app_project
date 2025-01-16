6d3560f48a7e76431b7ebb825b76cc2e32a3dcb8
Gys-Pernatiy <guskovdaniil12@gmail.com>
2025-01-17 02:50:10 +0300

    fix nginx and front

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -39,29 +39,26 @@ schema_view = get_schema_view(
 )
 
 router = DefaultRouter()
-router.register(r'walks', WalkViewSet, basename='walk')
+router.register(r'api/walks', WalkViewSet, basename='walk')
 
 urlpatterns = [
-    # path('', main_page, name='main_page'),
     path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('walks/<int:pk>/finish/', WalkViewSet.as_view({'post': 'finish'}), name='walk_finish'),
-    # path('walks/<int:pk>/update/', WalkViewSet.as_view({'put': 'update'}), name='walk_update'),
-    path('energy/<int:telegram_id>/', get_energy, name='get_energy'),
-    path('tasks/', get_tasks, name='get_tasks'),
-    path('tasks/<int:task_id>/complete/', tasks_complete, name='tasks_complete'),
-    path('streak/history/<int:telegram_id>/', streak_history, name='streak_history'),
-    path('bonus/claim/<int:telegram_id>/', claim_daily_bonus, name='claim_daily_bonus'),
-    # path('stepometer/', stepometer, name='stepometer'),
-    path('statistics/<int:telegram_id>/', get_statistics, name='get_statistics'),
-    # path('global-statistics/<int:telegram_id>/', global_statistics, name='global_statistics'),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('docs.<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-formatted'),
+    path('api/walks/<int:pk>/finish/', WalkViewSet.as_view({'post': 'finish'}), name='walk_finish'),
+    path('api/energy/<int:telegram_id>/', get_energy, name='get_energy'),
+    path('api/tasks/', get_tasks, name='get_tasks'),
+    path('api/tasks/<int:task_id>/complete/', tasks_complete, name='tasks_complete'),
+    path('api/streak/history/<int:telegram_id>/', streak_history, name='streak_history'),
+    path('api/bonus/claim/<int:telegram_id>/', claim_daily_bonus, name='claim_daily_bonus'),
+    path('api/statistics/<int:telegram_id>/', get_statistics, name='get_statistics'),
+    path('api/check_unfinished/', check_unfinished, name='check_unfinished'),
+    path('api/top-referrals/<int:telegram_id>/', user_top_referrals, name='user_top_referrals'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger_docs'),
+    path('docs.<str:format>', schema_view.without_ui(cache_timeout=0), name='swagger_docs_formatted'),
     path('t/', TemplateView.as_view(template_name='webapp_test.html')),
-    path('api/walk/check_unfinished/', check_unfinished, name='check_unfinished'),
-    path('api/top-referrals/<int:telegram_id>/', user_top_referrals, name='user-top-referrals'),
-    path('logs/', LogView.as_view(), name='log-view'),
+    path('logs/', LogView.as_view(), name='log_view'),
 ]
+
 
 urlpatterns += router.urls
 print(f'URLS: {urlpatterns}')
